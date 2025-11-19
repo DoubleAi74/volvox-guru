0f8d5a1d8f44adf4ab1d410df74e0d308ff34a37
DoubleAi <lotus.seed.page@gmail.com>
2025-11-20 02:40:32 +0800

    dd

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider } from "@/context/AuthContext";
-import Header from "@/components/Header"; // 1. Import the Header
 
 const inter = Inter({ subsets: ["latin"] });
 
